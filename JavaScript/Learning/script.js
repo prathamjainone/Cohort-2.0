51f098c2e413614383fa753734dd6120fa491d66
Pratham Jain <jainpratham370@gmail.com>
2026-05-11 05:08:00 +0530

    first unit

---
+++ JavaScript/Learning/script.js
@@ -0,0 +1,29 @@
+/*Practice Zone
+. Declare your name and city using const , and your age using let .
+. Try this and observe the result:
+. Guess the output:
+. Create a const object and add a new key to it — does it work?
+. Try accessing a let variable before declaring it — what error do you see?
+. Change a const array by pushing a value. Will it throw an error?
+*/
+
+const name="pratham jain";
+const city="gwalior";
+let age=21;
+
+console.log(count);
+var count=22;
+
+const obje={name:"pratham"}
+
+console.log(obje);
+
+obje.age=21;
+console.log(obje);
+
+const arr=[1,2,3,4,5]
+console.log(arr);
+
+arr.push(6);
+console.log(arr);
+
