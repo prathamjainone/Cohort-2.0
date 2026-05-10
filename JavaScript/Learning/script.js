cc199b188072f28137af1a579f1745b4a426aa5b
Pratham Jain <jainpratham370@gmail.com>
2026-05-11 05:26:21 +0530

    second unit

--- JavaScript/Learning/script.js
+++ JavaScript/Learning/script.js
@@ -27,3 +27,21 @@ console.log(arr);
 arr.push(6);
 console.log(arr);
 
+console.log(null + 1);
+console.log("5" + 3);
+console.log("5" - 3);
+console.log(true + false);
+
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof 123n);
+
+
+function isEmpty(a) {
+    if (a == null || a === "") { //or a===null||a===""||a===undefined
+        return true;
+    }
+    return false;
+}
+
+console.log(isEmpty(""));
