cf1764fecb51043fa0b74b5cddd592830e4f4208
Pratham Jain <jainpratham370@gmail.com>
2026-05-16 18:06:28 +0530

    create elements

--- JavaScript/DOM Manipulation/script.js
+++ JavaScript/DOM Manipulation/script.js
@@ -1,17 +1,27 @@
-const decreaseBtn=document.getElementById("decrease");
-const increaseBtn=document.getElementById("increase");
-const resetBtn=document.getElementById("reset");
-const counter=document.querySelector("h1");
+// const decreaseBtn=document.getElementById("decrease");
+// const increaseBtn=document.getElementById("increase");
+// const resetBtn=document.getElementById("reset");
+// const counter=document.querySelector("h1");
 
 
-decreaseBtn.addEventListener("click",()=>{
-    counter.innerText--;
-})
+// decreaseBtn.addEventListener("click",()=>{
+//     counter.innerText--;
+// })
 
-increaseBtn.addEventListener("click",()=>{
-    counter.innerText++;
-})
+// increaseBtn.addEventListener("click",()=>{
+//     counter.innerText++;
+// })
 
-resetBtn.addEventListener("click",()=>{
-    counter.innerText=0;
-})
+// resetBtn.addEventListener("click",()=>{
+//     counter.innerText=0;
+// })
+
+const list=document.createElement("ul");
+list.innerText="Counting";
+
+for(let i=1;i<=3;i++){
+    const item=document.createElement("li");
+    item.innerText=`Item ${i}`
+    list.appendChild(item);
+}
+document.body.appendChild(list);
